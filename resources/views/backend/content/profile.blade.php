c0ed60c6bc40698d5a3ad3c3bdda4ab69ff91d69
fifel <fifel.mei.z@mail.ukrim.ac.id>
2026-03-02 18:04:01 +0700

    templating

---
+++ resources/views/backend/content/profile.blade.php
@@ -0,0 +1,8 @@
+@extends('backend.layout.main')
+@section('content')
+
+    <h1> PROFILE </h1>
+
+    isi content
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
+Route::get('/admin/profile', [\App\Http\Controllers\DashboardController::class, 'profile'])->name('dashboard.profile');
